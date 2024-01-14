7d8c482dca2e777e37d3fe17bd9743ec49456449
RishanSubagar <rishans48@gmail.com>
2024-01-14 01:46:18 -0500

    Connected joints

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,12 @@
+// Points for fingers
+const fingerJoints = {
+    thumb: [0,1,2,3,4],
+    index: [0,5,6,7,8],
+    middle: [0,9,10,11,12],
+    ring: [0,13,14,15,16],
+    pinky: [0,17,18,19,20]
+};
+
 // Drawing function
 export const drawHand = (predictions, ctx) =>{
     if (predictions.length > 0) {
@@ -5,6 +14,33 @@ export const drawHand = (predictions, ctx) =>{
         predictions.forEach(prediction => {
             // Get landmarks
             const landmarks = prediction.landmarks;
+
+            // Loop through fingers
+            for (let j=0; j<Object.keys(fingerJoints).length; j++) {
+                // Loop through pairs of joints
+                let finger = Object.keys(fingerJoints)[j];
+                for (let k=0; k<fingerJoints[finger].length -1; k++) {
+                    // Get pairs of joints
+                    const firstJointIndex = fingerJoints[finger][k];
+                    const secondJointIndex = fingerJoints[finger][k+1];
+
+                    // Draw path
+                    ctx.beginPath();
+                    ctx.moveTo(
+                        landmarks[firstJointIndex][0],
+                        landmarks[firstJointIndex][1],
+                    );
+                    ctx.lineTo(
+                        landmarks[secondJointIndex][0],
+                        landmarks[secondJointIndex][1],
+                    )
+                    ctx.strokeStyle = "crimson"
+                    ctx.lineWidth = 4
+                    ctx.stroke();
+
+                }
+
+            }
 
             // Loop through landmarks and draw them
             for (let i=0; i<landmarks.length; i++) {
